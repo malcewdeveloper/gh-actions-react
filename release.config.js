b26945508389730cb5f4fff6c800d67c44624942
Maksim <malcewdeveloper@yandex.ru>
2023-10-18 17:42:04 +0300

    feat: updates release config

--- release.config.js
+++ release.config.js
@@ -1,14 +1,22 @@
 const config = {
-    branches: ['main', 'develop'],
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        ["@semantic-release/git", {
-            "assets": ["dist/*.js", "dist/*.js.map"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }],
-        '@semantic-release/github'
-    ]
+    "release": {
+      "branches": ["main"],
+      "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/npm",
+        "@semantic-release/github",
+        [
+          "@semantic-release/exec",
+          {
+            "prepareCmd": "echo 'Preparing MVP release'"
+          }
+        ]
+      ],
+      "extends": "@semantic-release/npm",
+      "tagFormat": "${version}-mvp.${next(0)}"
+    }
 };
 
 module.exports = config;
