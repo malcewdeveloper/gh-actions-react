b19a0cb0656aec177c0c01f26d017fbf1d8ba928
Maksim <malcewdeveloper@yandex.ru>
2023-10-18 23:32:03 +0300

    feat: create anything component

---
+++ src/component/AnythingThree/AnithingThree.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const AnythingThree = (props) => {
+    return (
+        <div>
+            { props.children }
+        </div>
+    )
+}
+
+export default AnythingThree;
