164d9f8828d5db007a964523893d1427239b8845
Maksim <malcewdeveloper@yandex.ru>
2023-10-18 12:54:09 +0300

    fix: moves release config

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['main', 'develop'],
+    branches: ['main'],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
